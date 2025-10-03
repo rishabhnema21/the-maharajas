cf8f95ffe740620faa2b632814b345a09d0fbd18
Rishabh Nema <182422143+rishabhnema21@users.noreply.github.com>
2025-10-03 22:30:22 +0530

    feat(ui): mouse cursor effect impelemtation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
-import Hero from '../components/home/Hero'
-import Navigation from '../components/Navigation'
-import Intro from '../components/home/Intro'
+import React from 'react';
+import Hero from '../components/home/Hero';
+import Navigation from '../components/Navigation';
+import Intro from '../components/home/Intro';
+import { Fluid } from '@whatisjery/react-fluid-distortion';
+import { EffectComposer } from '@react-three/postprocessing';
+import { Canvas } from '@react-three/fiber';
 
 const Home = () => {
   return (
-    <main className='min-h-screen w-full bg-[#070302]'>
-        <Navigation/>
-        <Hero />
-        <Intro />
-    </main>
-  )
-}
+    <main className="relative min-h-screen w-full bg-[#070302] overflow-hidden">
+      <div className="pointer-events-none fixed top-0 left-0 w-full h-full z-30 opacity-40">
+        <Canvas
+          style={{
+            width: '100%',
+            height: '100%',
+            background: 'transparent',
+          }}
+        >
+          <EffectComposer>
+            <Fluid
+              fluidColor="#808080"
+              rainbow={false}
+              showBackground={false}
+              intensity={20}
+              force={3}
+              distortion={2}
+              radius={0.3}
+              curl={10}
+              swirl={20}
+              velocityDissipation={0.99}
+              densitionDissipation={0.95}
+              pressure={0.80}
+            />
+          </EffectComposer>
+        </Canvas>
+      </div>
 
-export default Home
+
+      <Navigation />
+      <Hero />
+      <Intro />
+    </main>
+  );
+};
+
+export default Home;
