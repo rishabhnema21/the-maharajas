15f08c5e928aed31e0026028dfa2b4909589c795
Rishabh Nema <182422143+rishabhnema21@users.noreply.github.com>
2025-10-05 10:46:52 +0530

    update intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { ReactLenis, useLenis } from 'lenis/react'
 import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
 
 const App = () => {
+
+  const lenis = useLenis((lenis) => {
+    // called every scroll
+    console.log(lenis)
+  })
+
   return (
+    <>
+    <ReactLenis root />
     <Routes>
       <Route path='/' element={<Home />}/>
     </Routes>
+    </>
   )
 }
 
--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 
+const scenes = [
+  {
+    src: "https://ik.imagekit.io/c25vdtqqp/maharajas/chittorfort",
+    title: "The Dawn of Legends",
+    description:
+      "As the first light kisses the ancient walls of Chittor, echoes of courage awaken once more — where every stone whispers tales of valor that time could never silence.",
+  },
+  {
+    src: "https://ik.imagekit.io/c25vdtqqp/maharajas/palaces1.jpg",
+    title: "Where Royals Dreamed",
+    description:
+      "Beyond the marble corridors, the palaces breathe stories of grandeur - of laughter under chandeliers, of footsteps that once danced to the rhythm of royal grace.",
+  },
+  {
+    src: "https://ik.imagekit.io/c25vdtqqp/maharajas/palaces2.jpg",
+    title: "The Silent Twilight",
+    description:
+      "As dusk settles upon the domes, silence takes the throne - a silence not of emptiness, but of legacy, where the past and present bow to eternity together.",
+  },
+];
+
+
 const Intro = () => {
   return (
     <div className="p-2 md:p-6 bg-[#070302] w-full">
@@ -24,7 +46,7 @@ const Intro = () => {
             preserving the legacy of rulers and their reigns.
           </p>
         </div>
-        <div className="rounded-2xl w-[80vw] md:w-1/2 h-[20rem] md:h-[30rem] bg-amber-200 relative left-4 md:left-24 overflow-hidden">
+        <div className="rounded-2xl w-[80vw] md:w-1/2 h-[20rem] md:h-[30rem] relative left-4 md:left-24 overflow-hidden">
           <img
             src="/herobg.webp"
             className="object-cover object-bottom w-full h-full"
@@ -32,6 +54,30 @@ const Intro = () => {
           />
         </div>
       </div>
+
+      <div className="w-full flex flex-col items-center">
+      {scenes.map((scene, index) => (
+        <div
+          key={index}
+          className="h-screen w-full p-2 flex flex-col justify-center items-center md:p-6"
+        >
+          <div className="h-full md:w-2/3 relative mt-9 rounded-2xl overflow-hidden">
+            <img
+              src={scene.src}
+              alt={scene.title}
+              className="w-full h-full object-cover"
+            />
+
+            <div className="absolute inset-0 bg-gradient-to-tl from-black via-black/30 to-transparent"></div>
+
+            <div className="absolute w-2/3 text-end md:text-start md:w-1/2 bottom-5 md:bottom-1/7 right-4 md:right-10 font-[ethnic] text-white">
+              <h1 className="text-6xl md:text-5xl">{scene.title}</h1>
+              <p className="mt-4">{scene.description}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
     </div>
   );
 };
